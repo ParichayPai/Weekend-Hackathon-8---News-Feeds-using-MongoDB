f9130468184bba076236523c59450864329c114a
ParichayPai <parichaypai393@gmail.com>
2020-12-11 00:30:54 +0530

    update

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ const app = express()
 const port = 8080
 
 const onePageArticleCount = 10
-
+const {newsArticleModel} = require("./connector")
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get("/newsFeeds/:limit/:offset", (req, res) => {
+    let limit = parseInt(req.params.limit);
+    let offset = parseInt(req.params.offset);
+    if (isNaN(req.params.limit) || isNaN(req.params.offset)) {
+        limit = 10;
+        offset = 0;
+    }
+    newsArticleModel.find()
+    .limit(limit)
+    .skip(offset)
+    .then(data => res.send(data));
+})
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
